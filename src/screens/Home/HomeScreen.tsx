eb93cdfb23d7bea60efcde639303975004fb6d87
Sumit <ksumit1109@gmail.com>
2025-07-24 18:07:46 +0530

    Enhance HomeScreen with TextInput functionality
    - Added a TextInput component to allow user input on the HomeScreen.
    - Implemented state management for the input value using useState.
    - Commented out previous Text and Button components for future use.
    - Adjusted layout to position the TextInput at the bottom of the screen.

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import type { HomeScreenNavigationProp } from '@/types/navigation';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {View, Text, Button, TextInput} from 'react-native';
+import type {HomeScreenNavigationProp} from '@/types/navigation';
+import {useNavigation} from '@react-navigation/native';
 
 export default function HomeScreen() {
-    const navigation = useNavigation<HomeScreenNavigationProp>();
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  const [value, setValue] = useState('');
 
-    return (
-        <View className="flex-1 justify-center items-center">
-            <Text>Home Screen</Text>
-            <Button title="Go to Detail" onPress={() => navigation.navigate('Detail')} />
-        </View>
-    );
+  // Create the onChange function
+  const onChangeText = (text: string) => {
+    setValue(text);
+  };
+
+  return (
+    <View className="flex-1 justify-end items-center">
+      {/* <Text>Home Screen</Text> */}
+      {/* <Button
+        title="Go to Detail"
+        onPress={() => navigation.navigate('Detail')}
+      /> */}
+      <TextInput
+        editable
+        multiline
+        numberOfLines={4}
+        maxLength={40}
+        onChangeText={onChangeText}
+        value={value}
+        className="border border-gray-400 border-b-0 w-full rounded-t-lg px-2 min-h-28 max-h-40"
+      />
+    </View>
+  );
 }
